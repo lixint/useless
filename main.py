c13d4728617ed3d494ffef26acc73999a7f1a322
lixint8 <lixint8@163.com>
2018-12-28 15:10:38 +0800

    1

--- main.py
+++ main.py
@@ -28,16 +28,25 @@ import json
 def a():
 
 	url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=2019-01-16&leftTicketDTO.from_station=QTP&leftTicketDTO.to_station=WFK&purpose_codes=ADULT'
-	head = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-		'Accept-Encoding': 'gzip, deflate',
-		'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-		'Cache-Control': 'max-age=0',
-		'Connection': 'keep-alive',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+	'''head = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+					'Accept-Encoding': 'gzip, deflate',
+					'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+					'Cache-Control': 'max-age=0',
+					'Connection': 'keep-alive',
+					'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+					}'''
+
+	head = {'Connection': 'keep-alive',
+		'Cache-Control': 'no-cache',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+		'Accept':'*/*',
+		'X-Requested-With': 'XMLHttpRequest',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8'
 		}
 	with requests.Session() as s:
 		res = s.get(url,headers = head)
 	res.encoding = 'utf-8'
 	print(res.text)
 	return res
-#a = a()
+a = a()
