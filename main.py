c29b1e694b2279d4c8ab2d742dc5e5a73a2a38c3
lixint8 <lixint8@163.com>
2018-12-28 15:02:16 +0800

    1

--- main.py
+++ main.py
@@ -8,8 +8,8 @@
 import requests
 import json
 
-def a():
-	ltrain_date='2019-01-03'  #日期
+
+'''	ltrain_date='2019-01-03'  #日期
 	from_station='QTP' 	#出站
 	to_station='WFK'   #到站
 
@@ -24,15 +24,19 @@ def a():
 	"leftTicketDTO.to_station":to_station,
 	"purpose_codes":purpose_codes
 	}
+'''	
+def a():
 
+	url = 'https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=2019-01-16&leftTicketDTO.from_station=QTP&leftTicketDTO.to_station=WFK&purpose_codes=ADULT'
 	head = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-	'Accept-Encoding': 'gzip, deflate',
-	'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-	'Cache-Control': 'max-age=0',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+		'Accept-Encoding': 'gzip, deflate',
+		'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+		'Cache-Control': 'max-age=0',
+		'Connection': 'keep-alive',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+		}
 	with requests.Session() as s:
-		res = s.get(url,params=param,headers = head)
+		res = s.get(url,headers = head)
 	res.encoding = 'utf-8'
 	print(res.text)
 	return res
